0e70ab3bd76a995eb88e2c751ced8625f192c3e7
yangttian.yt <yangttian.yt@alipay.com>
2018-03-07 16:36:18 +0800

    更新 aapt与keystore

--- tools/mf.py
+++ tools/mf.py
@@ -14,12 +14,16 @@ if __name__ == '__main__':
     source_apk = sys.argv[1].strip()
     target_apk = sys.argv[2].strip()
     bundleFile = sys.argv[3].strip()
+
     # #source_apk = '/Users/tianyang/AndroidStudioProjects/MyApplication/applib2/build/outputs/apk/applib2-debug.apk'
     # #target_dir = '/Users/tianyang/AndroidStudioProjects/MyApplication/applib2/build/outputs/apk/applib2-bundle-debug.apk'
 
 
     shutil.copy(source_apk,  target_apk)
     zipped = zipfile.ZipFile(target_apk, 'a', zipfile.ZIP_DEFLATED)
-    zipEntryName = "META-INF/BUNDLE.MF"
+    if len(sys.argv) == 4:
+        zipEntryName = "META-INF/BUNDLE.MF"
+    else:
+        zipEntryName = sys.argv[4].strip()
     zipped.write(bundleFile, zipEntryName)
     zipped.close()
